1f50df9ddd8bee2e951c138db42f925035aa1af6
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-07-07 01:15:15 +0530

    var keyword

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Modern Javascript Crash Course
+// var, let  and const
+
+var age = 30;
+if (true) {
+    var age = 90;
+    console.log('Inside: ', age);
+}
+console.log('Outside: ', age);
+
+// Inside:  90
+// Outside:  90
