f45d2dbeaf8f8bc4954e7969e5d14ce2faad4126
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-07-07 02:17:08 +0530

    Destructuring - Object

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Modern Javascript Crash Course
-// Spread operator [...]
-
-const showValues = (...args) => {
-    // console.log(...args);
-    for (i = 0; i < args.length; i++) {
-        console.log(args[i]);
-    }
+// Destructuring
+let person = {
+    fname: 'VedaGna',
+    lname: 'Yarasu',
+    age: 5,
+    weight: 11,
 }
-showValues('Apples', 'Bananas', 'Oranges')
+const { fname, lname } = person;
+console.log(fname, lname); // VedaGna Yarasu
